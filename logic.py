c5797628a2b8bb855d5b952365cd8976759b736f
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-05-19 08:50:47 +0000

    Phone number logic added
    Here i adde the functionality for the Phone numbers and unfortunately am not getting the country codes at all and to make it valid you have to sepate the numbers with the comma or simply by dot.
    
    And also the numbers have to nine without including the country codes.
    
    And as addition to that, i added `\b` at the beginning and end of the patterns so that we can be able to allocate where to validate without getting stuck in sentences.

--- logic.py
+++ logic.py
@@ -3,29 +3,37 @@ import re
 # Adding Email extraction logic 
 def extract_emails(text):
     "Extracts email addresses from the given text."
-    pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
     return re.findall(pattern, text)
 
 # Adding Url extraction logic
 def extract_urls(text):
     "Extracts URLs from the given text."
-    pattern = r'https?://(?:www\.)?[\w-]+\.[\w.-]+[/\w .-]*'
+    pattern = r'\bhttps?://(?:www\.)?[\w-]+\.[\w.-]+[/\w .-]*\b'
     return re.findall(pattern, text)
 
+def extract_phone_numbers(text):
+    "Extracts phone numbers from the given text."
+    pattern = r'\b(?:\(\d{3}\)[\s-]?|\d{3}[.-]?)\d{3}[.-]?\d{3}\b'
+    return re.findall(pattern, text)
 
 def main():
     # For testing while using `re` package
     test_email = "albertniyon@gmail.com or a.niyonseng@alustudent.com"
     test_url = """https://rubular.com/, despite of using only that url let's try https://www.ucll.be/en
       or simply use this url to youtube video of It's complicated by Fave here https://www.youtube.com/watch?v=0Z7RjfeQaNk&pp=0gcJCdgAo7VqN5tD"""
+    
+    test_phone = "(250) 790 068 175 my number or this (250) 790-068-175 or simply with dot (250) 790.068.175"
 
     # Extracting 
     emails = extract_emails(test_email)
     urls = extract_urls(test_url)
+    phone = extract_phone_numbers(test_phone)
 
     # Printing the results
     print("Extracted Emails:", emails)
     print("Extracted Urls:", urls)
+    print("Extracted Phone number:", phone)
 
 if __name__ == "__main__":
     main()
