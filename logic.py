fe47138b28c16d6fe703bb27c2c8d343a893b6dd
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-05-19 08:11:20 +0000

    Url logic added
    Here i tried to add the pattern to check for urls and so far it's working even if it's not working well for all the urls.
    
    Example can be ` https://www.youtube.com/watch?v=0Z7RjfeQaNk&pp=0gcJCdgAo7VqN5tD` where am not getting all the url as it's and when all emails are incorporated in one text it tends not to extract them all.

--- logic.py
+++ logic.py
@@ -1,17 +1,30 @@
 import re
 
+# Adding Email extraction logic 
 def extract_emails(text):
     "Extracts email addresses from the given text."
     pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
     return re.findall(pattern, text)
 
+# Adding Url extraction logic
+def extract_urls(text):
+    "Extracts URLs from the given text."
+    pattern = r'https?://(?:www\.)?[\w-]+\.[\w.-]+[/\w .-]*'
+    return re.findall(pattern, text)
+
 
 def main():
     # For testing while using `re` package
-    test = "albertniyon@gmail.com or a.niyonseng@alustudent.com"
+    test_email = "albertniyon@gmail.com or a.niyonseng@alustudent.com"
+    test_url = "https://rubular.com/ or https://www.ucll.be/en"
 
-    # Extracting email
-    emails = extract_emails(test)
+    # Extracting 
+    emails = extract_emails(test_email)
+    urls = extract_urls(test_url)
+
+    # Printing the results
     print("Extracted Emails:", emails)
+    print("Extracted Urls:", urls)
+    
 if __name__ == "__main__":
     main()
